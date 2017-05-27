60c5efacc729cad17a6296a1c48710d2dfcf0968
Chris Vig <chris@invictus.so>
2017-05-27 17:18:26 -0500

    Initial state machine skeleton.

--- src/client.rkt
+++ src/client.rkt
@@ -37,10 +37,68 @@
     (close-output-port output-port)
     (client-log "Client connection terminated.")))
 
-;; -- Private Procedures --
+;; -- Private Procedures (State Machine) --
 
 (define (client-loop client)
-  (void))
+  (match (client-info-state client)
+    ;; Initial startup state.
+    ['start (client-loop (handle-start client))]
+    ;; Read request line.
+    ['get-request-line (client-loop (handle-get-request-line client))]
+    ;; Read header line.
+    ['get-header (client-loop (handle-get-header client))]
+    ;; Read request body.
+    ['get-body (client-loop (handle-get-body client))]
+    ;; Process request.
+    ['process-request (client-loop (handle-process-request client))]
+    ;; Send response
+    ['send-response (client-loop (handle-send-response client))]
+    ;; State machine is complete - return.
+    ['done (void)]
+    ;; Unknown state - should not happen.
+    [else (error "Invalid state!" (client-info-state client))]))
+
+(define (handle-start client)
+  (update-client-state client 'get-request-line))
+
+(define (handle-get-request-line client)
+  (let ([line (read-line-from-client client)])
+    (if line
+        (begin
+          (displayln line)
+          (update-client-state client 'get-header))
+        (update-client-state client 'done))))
+
+(define (handle-get-header client)
+  (update-client-state client 'get-body))
+
+(define (handle-get-body client)
+  (update-client-state client 'process-request))
+
+(define (handle-process-request client)
+  (update-client-state client 'send-response))
+
+(define (handle-send-response client)
+  (update-client-state client 'done))
+
+;; -- Private Procedures (Helpers) --
+
+(define (read-line-from-client client)
+  (let ([evt (sync (read-line-evt (client-info-input-port client))
+                   (thread-receive-evt))])
+    (match evt
+      ;; Client sent a line of text.
+      [(? string? line) line]
+      ;; Client disconnected from remote end.
+      [(? (λ (evt) (equal? evt eof)) _) #f]
+      ;; Received shutdown command
+      [(? (λ (evt) (equal? evt (thread-receive-evt))) _)
+       (match (thread-receive)
+         ['shutdown #f])])))
+
+(define (update-client-state client state)
+  (client-log "State is now: ~A" state)
+  (struct-copy client-info client [state state]))
 
 (define client-log
   (create-local-log "Client"))
