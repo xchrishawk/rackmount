af78c8b67adb1e274e70d54547e73926f24202f4
Chris Vig <chris@invictus.so>
2017-06-02 20:26:05 -0500

    Manager code cleanup.

--- src/main/manager-state.rkt
+++ src/main/manager-state.rkt
@@ -7,9 +7,9 @@
 ;;
 ;; The general structure is:
 ;;
-;; - opaque-manager-state
-;;   - worker-lookup (hash of worker-identifier? -> opaque-worker-state)
-;; - opaque-worker-state
+;; - manager-state
+;;   - worker-lookup (hash of worker-identifier? -> worker-state)
+;; - worker-state
 ;;   - worker (worker?)
 ;;   - task-lookup (hash of gen:task-identifer? -> gen:task-handle)
 ;;
@@ -27,93 +27,101 @@
  (contract-out
 
   ;; Creates a new manager state object.
-  [manager-state (-> (listof worker?) opaque-manager-state?)]
+  [rename make-manager-state manager-state (-> (listof worker?) manager-state?)]
 
   ;; Selects the appropriate worker for the next ask.
-  [manager-state-select-worker (-> opaque-manager-state? worker?)]
+  [manager-state-select-worker (-> manager-state? worker?)]
 
   ;; Gets the worker with the specified identifier.
-  [manager-state-get-worker (-> opaque-manager-state? worker-identifier? worker?)]
+  [manager-state-worker (-> manager-state? worker-identifier? worker?)]
 
   ;; Gets a list of all workers.
-  [manager-state-get-workers (-> opaque-manager-state? (listof worker?))]
+  [manager-state-workers (-> manager-state? (listof worker?))]
+
+  ;; Gets a list of the get events for all workers.
+  [manager-state-worker-get-evts (-> manager-state? (listof evt?))]
 
   ;; Adds a new task handle to the specified worker.
-  [manager-state-add-task-handle (-> opaque-manager-state?
+  [manager-state-add-task-handle (-> manager-state?
                                      worker-identifier?
                                      gen:task-handle?
-                                     opaque-manager-state?)]
+                                     manager-state?)]
 
   ;; Removes a task handle from the specified worker.
-  [manager-state-remove-task-handle (-> opaque-manager-state?
+  [manager-state-remove-task-handle (-> manager-state?
                                         worker-identifier?
                                         gen:task-identifier?
-                                        opaque-manager-state?)]
+                                        manager-state?)]
 
   ;; Gets the task handle with the specified identifier from the specified worker.
-  [manager-state-get-task-handle (-> opaque-manager-state?
+  [manager-state-get-task-handle (-> manager-state?
                                      worker-identifier?
                                      gen:task-identifier?
                                      gen:task-handle?)]))
 
 ;; -- Structs --
 
-(struct opaque-manager-state (worker-lookup))
+(struct manager-state (worker-lookup))
 
-(struct opaque-worker-state (worker task-lookup))
+(struct worker-state (worker task-lookup))
 
 ;; -- Public Procedures --
 
-(define (manager-state worker-list)
-  (opaque-manager-state
+(define (make-manager-state worker-list)
+  (manager-state
    (for/hash ([worker (in-list worker-list)])
      (values (worker-identifier worker)
-             (opaque-worker-state worker (hash))))))
+             (worker-state worker (hash))))))
 
-(define (manager-state-select-worker manager-state)
+(define (manager-state-select-worker state)
   ;; For now, just pick one at random. TODO: better selection
-  (let ([worker-states (hash-values (opaque-manager-state-worker-lookup manager-state))])
-    (opaque-worker-state-worker (list-ref worker-states (random (length worker-states))))))
+  (let ([worker-states (hash-values (manager-state-worker-lookup state))])
+    (worker-state-worker (list-ref worker-states (random (length worker-states))))))
 
-(define (manager-state-get-worker manager-state worker-identifier)
-  (let* ([worker-lookup (opaque-manager-state-worker-lookup manager-state)]
+(define (manager-state-worker state worker-identifier)
+  (let* ([worker-lookup (manager-state-worker-lookup state)]
          [worker-state (hash-ref worker-lookup worker-identifier)])
-    (opaque-worker-state-worker worker-state)))
+    (worker-state-worker worker-state)))
 
-(define (manager-state-get-workers manager-state)
-  (map opaque-worker-state-worker (hash-values (opaque-manager-state-worker-lookup manager-state))))
+(define (manager-state-workers state)
+  (map
+   worker-state-worker
+   (hash-values (manager-state-worker-lookup state))))
 
-(define (manager-state-add-task-handle manager-state worker-identifier task-handle)
-  (let* ([old-worker-lookup (opaque-manager-state-worker-lookup manager-state)]
+(define (manager-state-worker-get-evts state)
+  (map worker-get-evt (manager-state-workers state)))
+
+(define (manager-state-add-task-handle state worker-identifier task-handle)
+  (let* ([old-worker-lookup (manager-state-worker-lookup state)]
          [old-worker-state (hash-ref old-worker-lookup worker-identifier)]
          [new-worker-state (worker-state-add-task-handle old-worker-state task-handle)]
          [new-worker-lookup (hash-set old-worker-lookup worker-identifier new-worker-state)])
-    (struct-copy opaque-manager-state manager-state [worker-lookup new-worker-lookup])))
+    (struct-copy manager-state state [worker-lookup new-worker-lookup])))
 
-(define (manager-state-remove-task-handle manager-state worker-identifier task-handle-identifier)
-  (let* ([old-worker-lookup (opaque-manager-state-worker-lookup manager-state)]
+(define (manager-state-remove-task-handle state worker-identifier task-handle-identifier)
+  (let* ([old-worker-lookup (manager-state-worker-lookup state)]
          [old-worker-state (hash-ref old-worker-lookup worker-identifier)]
          [new-worker-state (worker-state-remove-task-handle old-worker-state task-handle-identifier)]
          [new-worker-lookup (hash-set old-worker-lookup worker-identifier new-worker-state)])
-    (struct-copy opaque-manager-state manager-state [worker-lookup new-worker-lookup])))
+    (struct-copy manager-state state [worker-lookup new-worker-lookup])))
 
-(define (manager-state-get-task-handle manager-state worker-identifier task-handle-identifier)
-  (let* ([worker-lookup (opaque-manager-state-worker-lookup manager-state)]
+(define (manager-state-get-task-handle state worker-identifier task-handle-identifier)
+  (let* ([worker-lookup (manager-state-worker-lookup state)]
          [worker-state (hash-ref worker-lookup worker-identifier)])
     (worker-state-get-task-handle worker-state task-handle-identifier)))
 
 ;; -- Private Procedures --
 
-(define (worker-state-add-task-handle worker-state task-handle)
-  (let* ([old-task-lookup (opaque-worker-state-task-lookup worker-state)]
+(define (worker-state-add-task-handle state task-handle)
+  (let* ([old-task-lookup (worker-state-task-lookup state)]
          [new-task-lookup (hash-set old-task-lookup (gen:task-handle-identifier task-handle) task-handle)])
-    (struct-copy opaque-worker-state worker-state [task-lookup new-task-lookup])))
+    (struct-copy worker-state state [task-lookup new-task-lookup])))
 
-(define (worker-state-remove-task-handle worker-state task-handle-identifier)
-  (let* ([old-task-lookup (opaque-worker-state-task-lookup worker-state)]
+(define (worker-state-remove-task-handle state task-handle-identifier)
+  (let* ([old-task-lookup (worker-state-task-lookup state)]
          [new-task-lookup (hash-remove old-task-lookup task-handle-identifier)])
-    (struct-copy opaque-worker-state worker-state [task-lookup new-task-lookup])))
+    (struct-copy worker-state state [task-lookup new-task-lookup])))
 
-(define (worker-state-get-task-handle worker-state task-handle-identifier)
-  (let* ([task-lookup (opaque-worker-state-task-lookup worker-state)])
+(define (worker-state-get-task-handle state task-handle-identifier)
+  (let* ([task-lookup (worker-state-task-lookup state)])
     (hash-ref task-lookup task-handle-identifier)))
--- src/main/manager.rkt
+++ src/main/manager.rkt
@@ -48,86 +48,80 @@
 ;; Main procedure for the thread.
 (define (manager-proc config)
   (manager-log-trace "Manager thread started.")
-  ;; Launch workers
-  (let* ([worker-list (for/list ([index (in-range (manager-config-worker-count config))])
-                        (let ([identifier (format "Worker ~A" index)])
-                          (manager-log-trace "Starting worker with identifier ~A..." identifier)
-                          (worker-start identifier)))]
-         [worker-get-evts (map worker-get-evt worker-list)])
-    ;; Enter main loop
-    (let loop ([state (manager-state worker-list)])
-      ;; Get the next event
-      (let ([evt (apply sync (wrapped-thread-receive-evt) worker-get-evts)])
-        (match evt
-          ;; Task to enqueue - select a worker and send it
-          [(? gen:task-handle? task-handle)
-           (loop (manager-proc-task-handle state task-handle))]
-          ;; Message from worker
-          [(list 'worker-message (? string? worker-identifier) message)
-           (loop (manager-proc-worker-message state worker-identifier message))]
-          ;; Shutdown command
-          ['shutdown (manager-proc-shutdown state)]
-          ;; Unknown event? Log and continue
-          [else (loop (manager-proc-unknown state))])))
-  ;; Log and shut down
-  (manager-log-trace "Manager thread terminating.")))
+  (let loop ([state (make-manager-state config)])
+    (sync
+     ;; Received thread message
+     (handle-evt
+      (wrapped-thread-receive-evt)
+      (match-lambda
+        ;; Task to enqueue
+        [(? gen:task-handle? task-handle)
+         (let ([task-handle-list (gen:task-handle->list task-handle)]
+               [worker (manager-state-select-worker state)])
+           (manager-log-trace "Queuing task ~A on worker ~A..."
+                              (gen:task-handle-identifier task-handle)
+                              (worker-identifier worker))
+           (worker-put worker task-handle-list)
+           (loop (manager-state-add-task-handle
+                  state
+                  (worker-identifier worker)
+                  task-handle)))]
+        ;; Shutdown command - stop looping
+        ['shutdown (void)]
+        ;; Unknown message?
+        [unrecognized-message
+         (manager-log-error
+          "Received unknown thread message (~A). Ignoring and continuing..."
+          unrecognized-message)
+         (loop state)]))
+     ;; Received worker message
+     (handle-evt
+      (apply choice-evt (manager-state-worker-get-evts state))
+      (match-lambda
+        ;; Log event from worker
+        [(list 'worker-message
+               (? worker-identifier? worker-identifier)
+               (? log-event-list? log-event-list))
+         (let ([log-event (list->log-event log-event-list)])
+           (log-event-enqueue log-event)
+           (loop state))]
+        ;; Task completed
+        [(list 'worker-message
+               (? worker-identifier? worker-identifier)
+               (list 'task-completed
+                     (? gen:task-identifier? task-identifier)))
+         (let ([task-handle (manager-state-get-task-handle
+                             state
+                             worker-identifier
+                             task-identifier)])
+           (manager-log-trace
+            "Received completion of task ~A from worker ~A. Closing and removing..."
+            task-identifier
+            worker-identifier)
+           (gen:task-handle-close task-handle)
+           (loop
+            (manager-state-remove-task-handle
+             state
+             worker-identifier
+             task-identifier)))]
+        ;; Unrecognized message
+        [unrecognized-message
+         (manager-log-error
+          "Received unknown worker message (~A). Ignoring and continuing..."
+          unrecognized-message)
+         (loop state)]))))
+  ;; Log shutdown
+  (manager-log-trace "Manager thread terminating."))
 
-;; -- Private Procedures (Event Handlers) --
+;; -- Private Procedures --
 
-;; Process a received task handle.
-(define (manager-proc-task-handle state task-handle)
-  (let ([task-handle-list (gen:task-handle->list task-handle)]
-        [worker (manager-state-select-worker state)])
-    (manager-log-trace "Queuing task ~A on worker ~A..."
-                       (gen:task-handle-identifier task-handle)
-                       (worker-identifier worker))
-    (worker-put worker task-handle-list)
-    (manager-state-add-task-handle state (worker-identifier worker) task-handle)))
-
-;; Process a message received from a worker.
-(define (manager-proc-worker-message state worker-identifier message)
-  (match message
-    ;; Message is a logged event
-    [(? log-event-list? log-event-list)
-     (manager-proc-worker-message-log-event state worker-identifier log-event-list)]
-    ;; Message is a "task completed" message
-    [(list 'task-completed task-handle-identifier)
-     (manager-proc-worker-message-task-completed state worker-identifier task-handle-identifier)]
-    ;; Unknown message type?
-    [else (manager-proc-worker-message-unknown state message)]))
-
-;; Process a shutdown command.
-(define (manager-proc-shutdown state)
-  (for ([worker (in-list (manager-state-get-workers state))])
-    (worker-stop worker)))
-
-;; Process an unknown event.
-(define (manager-proc-unknown state evt)
-  (manager-log-error "Received unknown event (~A). Ignoring and continuing..." evt)
-  state)
-
-;; -- Private Procedures (Worker Message Handlers) --
-
-;; Process a log event message.
-(define (manager-proc-worker-message-log-event state worker-identifier log-event-list)
-  (let ([log-event (list->log-event log-event-list)])
-    (log-event-enqueue log-event)
-    state))
-
-;; Process a task completed message.
-(define (manager-proc-worker-message-task-completed state worker-identifier task-handle-identifier)
-  (let ([task-handle (manager-state-get-task-handle state worker-identifier task-handle-identifier)])
-    (manager-log-trace "Received completion of task ~A from worker ~A. Closing and removing..."
-                       task-handle-identifier
-                       worker-identifier)
-    (gen:task-handle-close task-handle)
-    (manager-state-remove-task-handle state worker-identifier task-handle-identifier)))
-
-(define (manager-proc-worker-message-unknown state message)
-  (manager-log-error "Received unknown message (~A). Ignoring and continuing..." message)
-  state)
-
-;; -- Private Procedures (Utility) --
+;; Builds the manager state object with the specified number of workers.
+(define (make-manager-state config)
+  (manager-state
+   (for/list ([index (in-range (manager-config-worker-count config))])
+     (let ([identifier (format "Worker ~A" index)])
+       (manager-log-trace "Starting worker with identifier ~A..." identifier)
+       (worker-start identifier)))))
 
 ;; Local logging procedure.
 (define-local-log manager "Manager")
