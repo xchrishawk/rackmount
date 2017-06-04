b8fe3f1ecd7a78356e1e690eca3779257e46a971
Chris Vig <chris@invictus.so>
2017-06-03 19:29:46 -0500

    More worker worker.

--- src/tasks/worker.rkt
+++ src/tasks/worker.rkt
@@ -8,27 +8,25 @@
 
 #lang racket
 
-;; -- Requires --
-
-(require "../tasks/place.rkt")
-
 ;; -- Provides --
 
 (provide
  (contract-out
 
+  ;; -- Worker Management --
+
   ;; Creates a new worker.
   ;; - identifier: the identifier of the worker.
   ;; - output-channel: a channel to use to send messages to the manager.
-  [rename make-worker worker (->i ([identifier place-identifier?]
-                                   [place-to-manager-channel place-channel?])
-                                  [result worker?])]
+  [make-worker (->i ([identifier worker-identifier?]
+                     [place-to-manager-channel place-channel?])
+                    [result worker?])]
 
   ;; Predicate returning #t if the argument is a worker object.
   [worker? (-> any/c boolean?)]
 
   ;; Returns the identifier for the specified worker.
-  [worker-identifier (-> worker? place-identifier?)]
+  [worker-identifier (-> worker? worker-identifier?)]
 
   ;; Terminates a worker. If synchronous is #t, this method will block until
   ;; (worker-terminated-evt) is ready for synchronization.
@@ -38,26 +36,42 @@
 
   ;; Returns an event which is ready for synchronization when the place for the
   ;; specified worker is no longer running.
-  [worker-terminated-evt (-> worker? evt?)]))
+  [worker-terminated-evt (-> worker? evt?)]
+
+  ;; -- Utility --
+
+  ;; Predicate returning #t if the argument is a valid worker identifier.
+  [worker-identifier? (-> any/c boolean?)]))
+
+;; -- Parameters --
+
+(define current-worker-identifier
+  (make-parameter #f))
+
+(define current-worker-pch-to-manager
+  (make-parameter #f))
+
+(define current-worker-pch-from-manager
+  (make-parameter #f))
 
 ;; -- Structs --
 
 (struct worker (identifier place))
 
-;; -- Public Procedures --
+;; -- Public Procedures (Worker Management) --
 
-(define (make-worker identifier _pch-to-manager)
+(define (make-worker identifier pch-to-manager)
   ;; Create the place
-  (let ([pl (place _pch-from-manager
+  (let ([pl (place bootstrap-pch
               ;; Set place configuration
               (parameterize*
-                  ([pch-from-manager _pch-from-manager]
-                   [pch-to-manager (place-channel-get (pch-from-manager))]
-                   [place-identifier (place-channel-get (pch-from-manager))])
+                  ([current-worker-pch-from-manager bootstrap-pch]
+                   [current-worker-pch-to-manager (place-channel-get bootstrap-pch)]
+                   [current-worker-identifier (place-channel-get bootstrap-pch)])
                 ;; Run the main worker procedure
                 (worker-main)))])
     ;; Notify place of identifier and channel.
-    (place-channel-put pl _pch-to-manager)
+    (place-channel-put pl pch-to-manager)
     (place-channel-put pl identifier)
     ;; Return struct
     (worker identifier pl)))
@@ -66,7 +80,7 @@
   (place-channel-put (worker-place worker) message))
 
 (define (worker-terminate worker #:synchronous [synchronous #t])
-  (worker-send worker 'terminate)
+  (worker-put worker 'terminate)
   (when synchronous
     (sync (worker-terminated-evt worker)))
   (void))
@@ -76,17 +90,44 @@
    (place-dead-evt (worker-place worker))
    (λ (evt) worker)))
 
+;; -- Public Procedures (Utility) --
+
+(define worker-identifier? string?)
+
 ;; -- Private Procedures --
 
 (define (worker-main)
-  (displayln (format "TEMP: Worker (~A) started." (place-identifier)))
+  (displayln (format "TEMP: Worker (~A) started." (current-worker-identifier)))
   (let loop ()
     (sync
      (handle-evt
-      (pch-from-manager)
+      (current-worker-pch-from-manager)
       (match-lambda
         ['terminate (void)]
         [unrecognized-message
-         (displayln (format "I am ~A and I got: ~A" (place-identifier) unrecognized-message))
+         (displayln (format "I am ~A and I got: ~A" (current-worker-identifier) unrecognized-message))
          (loop)]))))
-  (displayln (format "TEMP: Worker (~A) terminated." (place-identifier))))
+  (displayln (format "TEMP: Worker (~A) terminated." (current-worker-identifier))))
+
+;; -- Tests --
+
+(module+ test
+
+  ;; -- Requires --
+
+  (require rackunit)
+
+  ;; -- Helpers --
+
+  (define (worker-and-pch [identifier "Example"])
+    (let*-values ([(our-pch their-pch) (place-channel)]
+                  [(worker) (make-worker identifier their-pch)])
+      (values worker our-pch)))
+
+  ;; -- Test Cases --
+
+  (test-case "Lifecycle"
+    (let-values ([(worker pch) (worker-and-pch)])
+      (check-false (sync/timeout 1.0 (worker-terminated-evt worker)))
+      (worker-terminate worker #:synchronous #f)
+      (check-equal? (sync/timeout 5.0 (worker-terminated-evt worker)) worker))))
